1b681aa3853632b3d062a5af69ff34ca35afa14c
grayowldev <truegrayowl@gmail.com>
2022-08-30 23:49:50 -0400

    extracted job information and connected to backend

--- background.js
+++ background.js
@@ -1,10 +1,44 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    console.log("this is a new tab with id "+ tabId)
-    const url = tab.url
-    console.log(url)
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//     console.log("this is a new tab with id "+ tabId)
+//     const url = tab.url
+//     console.log(url)
 
-    chrome.tab.sendMessage(tabId, {
-        type: "NEW",
-    })
+//     chrome.tab.sendMessage(tabId, {
+//         type: "NEW",
+//     })
 
-})
+// })
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['content.js']
+    });
+  });
+
+  chrome.runtime.onMessage.addListener((msg, sender, response) => {
+        console.log(sender.tab ? sender.tab.url :
+            "not a tab")
+        if (msg.req === "addJob" && msg.payload != {}) {
+            const endpoint = "http://localhost:7000/job/" 
+            console.log(endpoint);
+
+            fetch(endpoint).then((res) => {
+                if (res.status !== 200) {
+                    response({
+                        message: "Error",
+                    })
+                }
+                res.json().then( (data) => {
+                    response({
+                        message: "Success",
+                        payload: data});
+                }).catch((err) => {
+                    response({message: "Error"})
+                })
+            });
+            
+        }
+        return true
+    }
+  );
--- content.js
+++ content.js
@@ -12,13 +12,51 @@
         const jobInfo = jobData.children[0].children[0].children[0].children[0]
         const jobTitleHtml = jobInfo.children[0]
         const title = document.getElementsByClassName("t-24 t-bold jobs-unified-top-card__job-title")[0].childNodes
+        const company = document.getElementsByClassName("jobs-unified-top-card__company-name")[0].innerText
+        const location = document.getElementsByClassName("jobs-unified-top-card__bullet")[0].innerText
+        const workplaceType = document.getElementsByClassName("jobs-unified-top-card__workplace-type")[0] ? document.getElementsByClassName("jobs-unified-top-card__workplace-type")[0].innerText : "Ask"
+        const link = ""
+        const jobOrigin = ""
+        const jobId = ""
+        const companyImage = document.getElementsByClassName("ember-view link-without-hover-state inline-block")[0].innerHTML
+
         const exp = title[0];
-        const job = {
-        }
+        const job = {}
+        console.log(jobInfo);
+
+        console.log(company)
+        console.log(location)
+        console.log(workplaceType)
+        console.log(companyImage)
         console.log(jobTitleHtml);
         console.log(title);
-        console.log(exp);
+        // console.log(job);
+        console.log(exp)
+        job["title"] = exp
+        job["company"] = company
+        job["location"] = location
+        job["workplaceType"] = workplaceType
+        job["companyImage"] = cleanImageUrl(companyImage)
+        return job
+     }
+
+     const cleanImageUrl = (url) =>{
+        url = url.split('src="')[1].split('"')[0].replaceAll("&amp;","&")
+        console.log(url);
+        return url
      }
      
-     getJobData()
+     const job = getJobData()
+     job.title = job.title.textContent
+     console.log(job);
+
+     chrome.runtime.sendMessage({
+        req: "addJob",
+        payload: job,
+    }, (response) => {
+        console.log(response.message)
+        if ( response.message == "Success"){
+            console.log(response.payload);
+        }
+     })
 })();
